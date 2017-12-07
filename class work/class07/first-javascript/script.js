4322502a512fb326b3154bf9ad3ab2859bdd3c78
lilyou <lilyouch@gmail.com>
2017-12-06 20:45:23 -0500

    class 07 First Javascript

---
+++ class work/class07/first-javascript/script.js
@@ -0,0 +1,31 @@
+// console.log'this is a sweet status update');
+
+// console.log(10 * 10);
+// console.log(100/10);
+// console.log(50-17);
+// console.log(50+17);
+// console.log('Lily' + ' Ou');
+
+var product = 10 * 10;
+var quotient = 100/10;
+var difference = 50-17;
+var sum = 50+17;
+var name ='Lily' + ' Ou';
+
+console.log(product + quotient + difference + sum + name);
+
+function onra (){
+    var city = 'saigon'; 
+    console.log(`Last tango in $city`);
+
+}
+onra();
+onra();
+onra();
+
+// console.log(document.getElementById(hello));
+
+var hello = document.getElementById 
+('hello');
+
+hello.style.color = 'tomato';
