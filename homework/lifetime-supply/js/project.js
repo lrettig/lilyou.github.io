a38e2a47ea1ad3624b5e61908be832e64e1666ad
lilyou <lilyouch@gmail.com>
2017-12-16 16:23:41 -0500

    week 5  homework

--- homework/lifetime-supply/js/project.js
+++ homework/lifetime-supply/js/project.js
@@ -1,11 +1,22 @@
+$('#click-me').click(function(){
 // Store your current age into a variable
-
+var age = $('#age').val();
 // Store a maximum age into a variable
+var maxAge = $('#max-age').val();
 
 // Store a favorite drink (from a drop-down) into a variable
+var favoriteDrink = $('#item').val();
 
 // Store an amount per day into a variable
 
+var amountPerDay = $('#num-per-day').val();
+
 // Calculate how much you would drink for the rest of your life!
+var solution = ((maxAge - age) * amountPerDay * 365);
+console.log(solution);
 
 // Output your results to the user
+
+$('#output').html('You will drink ' + solution + ' more ' +  favoriteDrink + ' in your lifetime.');
+
+})
